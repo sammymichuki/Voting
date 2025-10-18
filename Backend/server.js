58fbe9dee1c4cd8b29502ba2909e794eb9de2530
sammymichuki <samichuki2022@gmail.com>
2025-10-18 13:41:42 +0300

    #

--- Backend/server.js
+++ Backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 const db = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'Samy@2248',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'evoting_system',
   waitForConnections: true,
   connectionLimit: 10,
